7091908b2ba50c987991dca0b5b78eb6e0f26957
Thanusan-Kanagenthiran <thanushmk6@gmail.com>
2023-02-06 14:24:33 +0530

    Category Edit and Delete functions added

--- src/main/java/com/cst18030/Category/CategoryController.java
+++ src/main/java/com/cst18030/Category/CategoryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -32,5 +33,17 @@ public class CategoryController {
 		repo.save(category);
 		return "redirect:/categories";
 	}
+	
+	@GetMapping("/categories/edit/{id}")
+	public String showCategoryEditForm(@PathVariable(name = "id") Integer id, Model model) {
+		Category category = repo.findById(id).orElse(null);
+		model.addAttribute("category", category);
+		return "category_form";
+	}
 
+	@GetMapping("/categories/delete/{id}")
+	public String deleteCategory(@PathVariable(name = "id") Integer id) {
+		repo.deleteById(id);
+		return "redirect:/categories";
+	}
 }
